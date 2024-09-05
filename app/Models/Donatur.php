44c7c968f01d500d18f8a7facc4abbe566850e99
irfanmustahafaa <irfanmustafa2307@gmail.com>
2024-09-05 16:04:47 +0700

    Add model, migration, orm

---
+++ app/Models/Donatur.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Donatur extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'phone_number',
+        'fundraising_id',
+        'total_amount',
+        'notes',
+        'proof',
+        'is_paid',
+    ];
+
+    public function fundraising()
+    {
+        return $this->belongsTo(Fundraising::class);
+    }
+}
---
+++ app/Models/Fundraising.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Fundraising extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'fundraiser_id',
+        'category_id',
+        'is_active',
+        'has_finished',
+        'name',
+        'slug',
+        'thumbnail',
+        'about',
+        'target_amount',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function fundraiser()
+    {
+        return $this->belongsTo(Fundraiser::class);
+    }
+
+    public function donaturs()
+    {
+        return $this->hasMany(Donatur::class)->where('is_paid', 1);
+    }
+
+    public function totalReachedAmount()
+    {
+        return $this->donaturs()->sum('total_amount');
+    }
+
+    public function withDrawals()
+    {
+        return $this->hasMany(FundraisingWithdrawal::class);
+    }
+}
